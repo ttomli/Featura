2a5de9236e69fc0c2344a4ff047b866e46b35d3c
EmperorBob7 <40090388+EmperorBob7@users.noreply.github.com>
2023-10-21 23:34:40 -0500

    Static round 7

--- featuraapi/featuraapi/settings.py
+++ featuraapi/featuraapi/settings.py
@@ -141,8 +141,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "rest_framework") 
+STATIC_URL = 'rest_framework/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static") 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
