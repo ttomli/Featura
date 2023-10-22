73adaeda1388976fdccc7a44e25a246e8ef91c7b
EmperorBob7 <40090388+EmperorBob7@users.noreply.github.com>
2023-10-22 09:56:29 -0500

    Static File Update

--- featuraapi/featuraapi/settings.py
+++ featuraapi/featuraapi/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'checkstatic/static/')
+   os.path.join(BASE_DIR, 'checkstatic/front/')
 ]
 # STATICFILES_DIRS = [BASE_DIR/'static/rest_framework']
 
